bead236c1123d24eed704f6e33b89454bcd3e8f9
ferhan6724 <ferhan6724@gmail.com>
2023-07-13 19:40:26 +0300

    playerCard

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -1,7 +1,7 @@
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+// import Col from 'react-bootstrap/Col';
 import { useState } from 'react';
 import {data} from "../helper/data"
 import PlayerCard from './PlayerCard';
@@ -23,10 +23,10 @@ const filteredData = data.filter(item =>item.name.toLowerCase().includes(search.
 
     <Container className='p-3 rounded-4 card card-container my-3'>
        
-        <Row xs={2} md={4} lg={6} className='justify-content-center g-3'>
-        <Col>1 of 3</Col>
+    <Row xs={2} md={4} lg={6} className='justify-content-center g-3'>
+        
         {filteredData.map((player,i)=><PlayerCard key={i} {...player} />)}
-      </Row>
+    </Row>
     </Container>
 
 
--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,6 +1,22 @@
-const PlayerCard = () => {
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+const PlayerCard = ({name, img, statistics}) => {
   return (
-    <div>PlayerCard</div>
+    
+    <Col>
+    <Card>
+        <Card.Img variant="top" src={img}  height="350px"/>
+        <ul> {
+            statistics.map((item,i)=> <li  className='list-unstyled h-5 text-start' key={i}>🏀{item}</li>)
+        }
+        </ul>
+       
+        <Card.Footer>
+        <Card.Title>{name}</Card.Title>
+        </Card.Footer>
+      </Card>
+    </Col>
   )
 }
 
