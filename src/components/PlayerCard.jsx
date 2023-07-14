1528fc8532a88ad7406f0dc856b760d49aabd58d
ferhan6724 <ferhan6724@gmail.com>
2023-07-14 13:26:36 +0300

    card onclick

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,23 +1,32 @@
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import { useState } from "react";
 
-const PlayerCard = ({name, img, statistics}) => {
+const PlayerCard = ({ name, img, statistics }) => {
+  const [showImage, setShowImage] = useState(true);
+
   return (
-    
     <Col>
-    <Card>
-        <Card.Img variant="top" src={img}  height="350px"/>
-        <ul> {
-            statistics.map((item,i)=> <li  className='list-unstyled h-5 text-start' key={i}>🏀{item}</li>)
-        }
-        </ul>
-       
+      <Card className="player-card" role="button" onClick={() => setShowImage(!showImage)}>
+        {showImage ? (
+          <Card.Img variant="top" src={img} height="350px" />
+        ) : (
+          <ul className="m-auto">
+            {" "}
+            {statistics.map((item, i) => (
+              <li className="list-unstyled h-5 text-start" key={i}>
+                🏀{item}
+              </li>
+            ))}
+          </ul>
+        )}
+
         <Card.Footer>
-        <Card.Title>{name}</Card.Title>
+          <Card.Title>{name}</Card.Title>
         </Card.Footer>
       </Card>
     </Col>
-  )
-}
+  );
+};
 
-export default PlayerCard
+export default PlayerCard;
